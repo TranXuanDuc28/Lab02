1ba172fe3509992faefeed10cdea044bd44c1012
LAPTOPMSI <ductx.22it@vku.udn.vn>
2025-09-13 15:20:22 +0700

    push code

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 2f;        // tốc độ di chuyển
+    public float moveDistance = 3f; // khoảng cách di chuyển
+
+    private Vector3 startPos;
+    private int direction = 1;
+
+    void Start()
+    {
+        startPos = transform.position; // lưu vị trí ban đầu
+    }
+
+    void Update()
+    {
+        // Di chuyển theo hướng hiện tại
+        transform.Translate(Vector2.right * direction * speed * Time.deltaTime);
+
+        // Nếu đi quá khoảng cách cho phép thì đổi hướng
+        if (Mathf.Abs(transform.position.x - startPos.x) >= moveDistance)
+        {
+            direction *= -1; // đổi chiều
+        }
+    }
+}
